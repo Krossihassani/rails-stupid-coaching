d537dbc8adcca51bc35186d18e83bfaebe3f2a11
kawtar rossi <kawtar.rossi@gmail.com>
2022-02-20 17:11:54 +0100

    stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
-    @laquestion = params[:question]
   end
 
   def answer
-    @laquestion = params[:question]
-    if @laquestion == 'I am going to work'
-      @answer = 'great'
-    elsif @laquestion.last == '?'
-      @answer = 'Silly question'
-    else @answer = 'I don\'t care, get dressed and go to work!.'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
